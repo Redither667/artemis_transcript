387779280f83b9642fc29d5c64de167378f23312
Redither667 <redither667@foxmail.com>
2026-04-18 23:40:52 +0800

    Add support for select block.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import functools
 from abc import ABC, abstractmethod
 from argparse import ArgumentParser
 from collections.abc import Callable
+from dataclasses import dataclass
+from enum import Enum, auto
 from pathlib import Path
 
 from lupa import LuaRuntime
@@ -18,7 +20,15 @@ class OutputFuncSet(ABC):
         ...
 
     @abstractmethod
-    def write_line(self, name: Optional[str], text: str):
+    def write_select_title(self, text: str):
+        ...
+
+    @abstractmethod
+    def write_text(self, name: Optional[str], text: str):
+        ...
+
+    @abstractmethod
+    def write_text_in_parenthesis(self, text: str):
         ...
 
     @abstractmethod
@@ -26,7 +36,27 @@ class OutputFuncSet(ABC):
         ...
 
 
-def parse_ast(ast_text: str, output_func_set: OutputFuncSet):
+def get_label_block(ast, label_name: str) -> str:
+    label = ast['label'][label_name]
+    assert label['label'] == 1
+    return label['block']
+
+
+class BlockType(Enum):
+    text = auto()
+    excall = auto()
+    select = auto()
+
+
+@dataclass
+class Selection:
+    label: str
+    exp: str
+    text: str
+    display: str
+
+
+def parse_ast(ast_text: str, output: OutputFuncSet):
     lua = LuaRuntime(unpack_returned_tuples=True)
     lua.execute(ast_text)
     if lua.globals().astver != 2.0:
@@ -36,47 +66,96 @@ def parse_ast(ast_text: str, output_func_set: OutputFuncSet):
         ast_name = 'ast'
     ast = lua.globals().__getattribute__(ast_name)
     try:
-        top_block = ast['label']['top']['block']
+        top_block = get_label_block(ast, 'top')
     except TypeError:
         raise ValueError('top block not found')
+
+    selection_flag: bool = False
+    selection_blocks: dict[str, Selection] = {}
+
     current_block = top_block
     while current_block is not None:
         block = ast[current_block]
+        block_type = BlockType.text
         for attr in block:
             assert type(attr) is int
             match block[attr][1]:
                 case 'savetitle':
-                    output_func_set.write_save_title(block[attr]['text'])
+                    output.write_save_title(block[attr]['text'])
+                case 'select':
+                    block_type = BlockType.select
                 case 'text':
                     break
                 case 'excall':
-                    with open(f'source/{block[attr]['file']}.ast', 'r', encoding='utf-8') as ex_f:
-                        parse_ast(ex_f.read(), output_func_set)
+                    if block[attr]['file'] is not None:
+                        with open(f'source/{block[attr]['file']}.ast', 'r', encoding='utf-8') as ex_file:
+                            parse_ast(ex_file.read(), output)
+                    elif block[attr]['label'] is not None and block[attr]['call'] == 0:
+                        block_type = BlockType.excall
                 case _:
                     pass  # TODO: parse other options
-        text = block['text']
-        try:
-            ja = text['ja'][1]
-        except TypeError:
-            current_block = block['linknext']
-            continue
-        if ja['name'] is not None:
-            name = ja['name'][2]
-        else:
-            name = None
-        for attr in ja:
-            if type(attr) is int:
-                if type(ja[attr]) is str:
-                    if ja[attr] == '　':
-                        break
-                    output_func_set.write_line(name, ja[attr])
+        match block_type:
+            case BlockType.text:
+                if selection_flag and current_block in selection_blocks.keys():
+                    output.write_select_title(selection_blocks[current_block].display)
+                text = block['text']
                 try:
-                    if ja[attr][1] == 'rt2':
-                        output_func_set.newline()
-                except TypeError and IndexError:
-                    pass
+                    ja = text['ja'][1]
+                except TypeError:
+                    current_block = block['linknext']
+                    continue
+                if ja['name'] is not None:
+                    name = ja['name'][2]
+                else:
+                    name = None
+                for attr in ja:
+                    if type(attr) is int:
+                        if type(ja[attr]) is str:
+                            if ja[attr] == '　':
+                                # output.write_text(None, '')
+                                break
+                            else:
+                                output.write_text(name, ja[attr])
+                        try:
+                            if ja[attr][1] == 'rt2':
+                                output.newline()
+                        except TypeError and IndexError:
+                            pass
 
-        current_block = block['linknext']
+                current_block = block['linknext']
+            case BlockType.excall:
+                current_block = block['linknext']
+            case BlockType.select:
+                selections: list[Selection] = []
+                for attr in block:
+                    assert type(attr) is int
+                    match block[attr][1]:
+                        case 'savetitle':
+                            output.write_save_title(block[attr]['text'])
+                        case 'select':
+                            try:
+                                selections.append(Selection(block[attr]['label'],
+                                                            block[attr]['text'],
+                                                            block[attr]['exp'], ''))
+                            except TypeError and IndexError:
+                                pass
+                        case 'text':
+                            break
+                        case _:
+                            pass
+
+                select = block['select']
+                ja = select['ja']
+                for attr, selection in zip(ja, selections):
+                    assert type(attr) is int and type(ja[attr]) is str
+                    output.write_text(None, f'→ {ja[attr]}')
+                    output.write_text_in_parenthesis(selection.exp)
+                    selection.display = ja[attr]
+                    selection_blocks[get_label_block(ast, selection.label)] = selection
+                    output.newline()
+                selection_flag = True
+                current_block = block['linknext']
+
 
 
 class MarkdownOutput(OutputFuncSet):
@@ -95,11 +174,19 @@ class MarkdownOutput(OutputFuncSet):
         self.file.write(f'## {text}\n')
 
     @override
-    def write_line(self, name: Optional[str], text: str):
+    def write_select_title(self, text: str):
+        self.file.write(f'### {text}\n')
+
+    @override
+    def write_text(self, name: Optional[str], text: str):
         if name is None:
             self.file.write(text)
         else:
             self.file.write(f'{name}：{text}')
+
+    @override
+    def write_text_in_parenthesis(self, text: str):
+        self.file.write(f'（*{text}*）')
 
     @override
     def newline(self):
