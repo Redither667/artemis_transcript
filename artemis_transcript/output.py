f935b49e84bbae5a85a4ad56314cf13de3331766
Redither667 <redither667@foxmail.com>
2026-04-19 18:49:30 +0800

    Add primitive docx support

--- artemis_transcript/output.py
+++ artemis_transcript/output.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from typing import override, Optional
 
+from docx import Document
+from docx.oxml.ns import qn
+from docx.text.paragraph import Paragraph
+
 from artemis_transcript.core import OutputFuncSet
 
 
@@ -47,3 +51,60 @@ class MarkdownOutput(OutputFuncSet):
     @override
     def newline(self):
         self.file.write('  \n')
+
+class DocxOutput(OutputFuncSet):
+    def __init__(self, file: Path):
+        self.path = file
+        self.document = Document()
+        self.paragraph = self.document.add_paragraph()
+        style = self.document.styles['Normal']
+        style.font.name = 'Times New Roman'
+        style.element.rPr.rFonts.set(qn('w:eastAsia'), '宋体')
+
+    def __del__(self):
+        self.document.save(str(self.path))
+
+    @override
+    def write_story_line(self, text: str):
+        title = self.document.add_heading(text, level=0)
+        title.alignment = 1
+        self.paragraph = self.document.add_paragraph()
+
+    @override
+    def write_save_title(self, text: str):
+        self.document.add_heading(text, level=1)
+        self.paragraph = self.document.add_paragraph()
+
+    @override
+    def write_select_title(self, text: str):
+        self.document.add_heading(text, level=2)
+        self.paragraph = self.document.add_paragraph()
+
+    @override
+    def write_text(self, name: Optional[str], text: str, face: Optional[str] = None):
+        if name is None:
+            assert face is None
+            self.paragraph.add_run(text)
+        else:
+            if face is None:
+                self.paragraph.add_run(f'{name}：{text}')
+            else:
+                self.paragraph.add_run(f'{name}:')
+                self.write_text_in_parenthesis(face)
+                self.paragraph.add_run(text)
+
+    @override
+    def write_text_in_parenthesis(self, text: str):
+        self.paragraph.add_run('（')
+        self.write_italic_text(text)
+        self.paragraph.add_run('）')
+
+    @override
+    def write_italic_text(self, text: str):
+        run = self.paragraph.add_run(text)
+        run.font.name = 'Times New Roman Italic'
+        run.element.rPr.rFonts.set(qn('w:eastAsia'), '楷体')
+
+    @override
+    def newline(self):
+        self.paragraph.add_run('').add_break()
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from artemis_transcript import *
 
-if __name__ == '__main__':
+
+def md_test():
     md_output = MarkdownOutput(Path('output/transcipted.md'), 'w', encoding='utf-8')
     md_output.write_story_line('共通线')
     with open('source/0_kyo_01.ast', 'r', encoding='utf-8') as f:
         parse_ast(f.read(), md_output)
-        # parse_ast('astver = 2.0\nast = {label = {top = {block = "1"}}}')
+
+
+def docx_test():
+    docx_output = DocxOutput(Path('output/transcipted.docx'))
+    docx_output.write_story_line('共通线')
+    with open('source/0_kyo_01.ast', 'r', encoding='utf-8') as f:
+        parse_ast(f.read(), docx_output)
+
+if __name__ == '__main__':
+    docx_test()
