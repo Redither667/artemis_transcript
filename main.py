6eada795a591d9c1125cf447b0e6f5788666b27a
Redither667 <redither667@foxmail.com>
2026-04-19 22:23:11 +0800

    Add a detailed example of Sayumi from Amakano 3 in main.py

--- main.py
+++ main.py
@@ -9,9 +9,24 @@ def md_test():
 
 
 def docx_test():
-    docx_output = DocxOutput(Path('output/transcipted.docx'))
+    docx_output = DocxOutput(Path('output/0-kyo.docx'))
     docx_output.write_story_line('共通线')
     with open('source/0_kyo_01.ast', 'r', encoding='utf-8') as f:
+        parse_ast(f.read(), docx_output)
+
+    docx_output = DocxOutput(Path('output/3-say-1.docx'))
+    docx_output.write_story_line('纱由美线Part Ⅰ')
+    with open('source/3_say_01-01.ast', 'r', encoding='utf-8') as f:
+        parse_ast(f.read(), docx_output)
+
+    docx_output = DocxOutput(Path('output/3-say-2.docx'))
+    docx_output.write_story_line('纱由美线Part Ⅱ')
+    with open('source/3_say_02-01.ast', 'r', encoding='utf-8') as f:
+        parse_ast(f.read(), docx_output)
+
+    docx_output = DocxOutput(Path('output/3-say-3.docx'))
+    docx_output.write_story_line('纱由美线Part Ⅲ')
+    with open('source/3_say_03-01.ast', 'r', encoding='utf-8') as f:
         parse_ast(f.read(), docx_output)
 
 if __name__ == '__main__':
